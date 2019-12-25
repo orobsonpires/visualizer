c7dc3ea371b9ea68157c600daac524cb3627ea78
None <orobsonpires@gmail.com>
2019-12-25 16:30:58 +0100

    add led control

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const micStream = require('mic-stream');
 const Through = require('audio-through');
+const Gpio = require('onoff').Gpio;
+
+var LED = new Gpio(4, 'out');
+var blinkInterval = setInterval(blinkLED, 250);
 
 var s = micStream();
 
@@ -46,3 +50,19 @@ function getbars(val) {
     }
     return bars;
 }
+
+function blinkLED() { //function to start blinking
+    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+        LED.writeSync(1); //set pin state to 1 (turn LED on)
+    } else {
+        LED.writeSync(0); //set pin state to 0 (turn LED off)
+    }
+}
+
+function endBlink() { //function to stop blinking
+    clearInterval(blinkInterval); // Stop blink intervals
+    LED.writeSync(0); // Turn LED off
+    LED.unexport(); // Unexport GPIO to free resources
+}
+
+setTimeout(endBlink, 5000);
